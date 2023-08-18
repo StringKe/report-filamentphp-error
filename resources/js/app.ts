d2063d83964380231f1419043ed29b070a470a4a
{authemail@qq.com} <authemail@qq.com>
2023-08-18 12:02:06 +0800

    feat: init filament

---
+++ resources/js/app.ts
@@ -0,0 +1,17 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+
+declare global {
+
+    interface Window {
+        Alpine: typeof Alpine;
+    }
+}
+
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
